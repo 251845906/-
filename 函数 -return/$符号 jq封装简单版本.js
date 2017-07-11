9451a27144d0e0576e764543efeb0235456d5a9e
251845906 <251845906@qq.com>
2017-07-11 18:13:06 +0800

    xc

---
+++ 函数 -return/$符号 jq封装简单版本.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/7/11 0011.
+ */
+$(function(){})
+$('#id')
+$('.aa')
+$(this)
+function $(v){
+	if (typeof v === 'function'){
+		window.onload = v
+	}else if (typeof v === 'string'){
+		var first = v.charAt(0);
+		switch (first){
+			case '#':
+				return document.getElementById(v.substring(1))
+				break;
+			case '.':
+				return document.getElementById(v.substring(1))
+				break;
+		}
+	}else if(typeof v === 'object'){
+		return v
+	}
+}
