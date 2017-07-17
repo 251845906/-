0cf83915d92342ba26c564f00168afcae23ef811
251845906 <251845906@qq.com>
2017-07-17 18:31:32 +0800

    xc

--- ku.js
+++ ku.js
@@ -6,20 +6,89 @@ $(function(){})
 $('#id')
 $('.aa')
 $(this)
-function $(v){
-	if (typeof v === 'function'){
-		window.onload = v
-	}else if (typeof v === 'string'){
-		var first = v.charAt(0);
-		switch (first){
-			case '#':
-				return document.getElementById(v.substring(1))
-				break;
-			case '.':
-				return document.getElementById(v.substring(1))
-				break;
-		}
-	}else if(typeof v === 'object'){
-		return v
-	}
-}
+    function $(v){
+        if (typeof v === 'function'){
+            window.onload = v
+        }else if (typeof v === 'string'){
+            var first = v.charAt(0);
+            switch (first){
+                case '#':
+                    return document.getElementById(v.substring(1))
+                    break;
+                case '.':
+                    return document.getElementById(v.substring(1))
+                    break;
+            }
+        }else if(typeof v === 'object'){
+            return v
+        }
+
+    };
+    function css(obj,property,price) {
+
+        if (arguments[2] === 'undefined'){
+            read();
+            return read;
+        }else{
+            write()
+        }
+        function read(){
+            if (window.getComputedStyle){
+                return document.getComputedStyle(obj,'null')[property];
+            }else{
+                return obj.currentStyle[property];
+            }
+        };
+        function write(){
+            property  = eval(property)
+            obj.style[property] = price;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+// function $() {
+//     var elements = [];
+//     for (var i = 0, len = arguments.length; i < len; i++) {
+//         var element = arguments[i];
+//         if (typeof element === 'string') {
+//             element = document.getElementById(element);
+//         }
+//         if (arguments.length !== 1) {
+//         } else {
+//             return element;
+//         }
+//         elements.push(element);
+//     }
+//     return elements;
+// };
+//
+//     $ = {
+//         css: function (a, b) {
+//             alert(this)
+//             arguments[1] === 'undefined' ? read(a) : write(a, b);
+//             function read(a) {
+//                 if (window.getComputedStyle) {
+//                     window.getComputedStyle('', null).a
+//                 } else {
+//                     ''.currentStyle.a;
+//                 }
+//                 ;
+//             };
+//             function write() {
+//
+//             };
+//         }
+//     }
+// window.$ = function(){
+//     return new $(arguments);
+// }
+
