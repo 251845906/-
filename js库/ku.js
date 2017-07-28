76e763303726cfa5447d1423c826d03fea48d245
251845906 <251845906@qq.com>
2017-07-28 18:55:12 +0800

    728

--- js库/ku.js
+++ js库/ku.js
@@ -7,19 +7,19 @@ $('#id')
 $('.aa')
 $(this)
 function $(v){
-	if (typeof v === 'function'){
-		window.onload = v
-	}else if (typeof v === 'string'){
-		var first = v.charAt(0);
-		switch (first){
-			case '#':
-				return document.getElementById(v.substring(1))
-				break;
-			case '.':
-				return document.getElementById(v.substring(1))
-				break;
-		}
-	}else if(typeof v === 'object'){
-		return v
-	}
+    if (typeof v === 'function'){
+        window.onload = v
+    }else if (typeof v === 'string'){
+        var first = v.charAt(0);
+        switch (first){
+            case '#':
+                return document.getElementById(v.substring(1))
+                break;
+            case '.':
+                return document.getElementById(v.substring(1))
+                break;
+        }
+    }else if(typeof v === 'object'){
+        return v
+    }
 }
